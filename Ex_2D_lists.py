21ef4a784b7487f7bbb8ff68e70797b2c557fe16
Peter. K <peterkolev73@gmail.com>
2022-03-30 12:47:46 -0500

    Add files via upload

---
+++ Ex_2D_lists.py
@@ -0,0 +1,13 @@
+# A simple introduction to 2D lists in python! 
+
+# This list will contain a list with 3 diffferent data types. A int, string and bool.
+
+#first Ex.
+Ex_Data = [[5, "ice Cream", True], [20, "ice Cream", False], [10, "candy", True], [25, "candy", False]]
+
+#Second Ex will instead be two strings and one bool.
+Ex_data_two = [["Alex", "Texas", True], ["peter", "Illinois", True], ["Austin", "Arizona", False]]
+
+Ex_data_two[2][2] = True
+
+print(Ex_data_two)
